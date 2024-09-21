d4b1b07c9476c55a165ce34a278ef1f21f8a0491
kamrulhasananik019 <kamrulhasananik019@gmail.com>
2024-09-21 23:52:34 +0600

    new add

---
+++ RelationalOperator.py
@@ -0,0 +1,7 @@
+
+a=int(30)
+b=int(20)
+
+result= a>b , a>=b, a==b, a<b,a<=b, a!=b, 'hello'=='hello'
+
+print(result)
---
+++ Type.py
@@ -0,0 +1,10 @@
+from operator import truth
+
+num= 20
+s='hello'
+b=20.0
+n=truth
+
+print(type(num), type(s), type(b) , end=' ')
+
+print(type(n))
